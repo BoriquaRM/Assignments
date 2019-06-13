c7accc6caf7188673e0be680c965a9e29aaa08b8
David <49924716+BoriquaRM@users.noreply.github.com>
2019-06-13 01:09:00 -0700

    Add files via upload

---
+++ lesson-8/js/main.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+	document.getElementsByClassName("navbutton")[0].classList.toggle("responsive");}
+
+function Date().toDateString()) {
+	var event = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	console.log(event.toLocaleDateString();
+	console.log(event.toLocaleDateString();
+	console.log(event.toLocaleDateString();
+	
+function whindChill() {
+	x = 35.74 + 0.6215\:t-35.75\:s^(0.16)+0.4275\:t\:s^(0.16);}
