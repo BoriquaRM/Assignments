6643d215b181f361454a60449ebfb61e12913c20
David <49924716+BoriquaRM@users.noreply.github.com>
2019-06-16 22:07:52 -0700

    Add files via upload

---
+++ lesson-9/js/currentdate.js
@@ -0,0 +1,7 @@
+function Date().toDateString()) {
+	var event = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	console.log(event.toLocaleDateString();
+	console.log(event.toLocaleDateString();
+	console.log(event.toLocaleDateString();
+}
